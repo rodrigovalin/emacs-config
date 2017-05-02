697bdce005fefaf11dbce6572fadd7b43029bc85
Rodrigo Valin <licorna@gmail.com>
2017-05-02 02:44:00 +0100

    Initial configuration based on use-package

---
+++ custom.el
@@ -0,0 +1,72 @@
+;;
+;; custom.el configuration file
+;; this is meant to be used with emacs-prelude
+;;
+;; Rodrigo Valin
+;;
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+;; Generic configuration
+;;
+
+;; disable emacs original movement keys
+(setq prelude-guru nil)
+
+;; menu bar is hidden
+(menu-bar-mode -1)
+
+;; disables visible bell
+(setq visible-bell t)
+
+;; initial scratch buffer should be empty
+(setq initial-scratch-message nil)
+
+;; makes scroll bar dissappear
+(toggle-scroll-bar 0)
+
+
+(use-package helm
+  :config (helm-mode 1)
+  :bind (("C-c h i" . helm-imenu)
+         ("C-c h o" . helm-occur)
+         ("C-c p f" . helm-projectile)))
+
+(use-package elpy
+  :ensure t
+  :config (elpy-enable))
+
+(use-package virtualenvwrapper
+  :ensure t
+  :init (setq venv-location "~/workspace/virtualenvs")
+  :config
+  (make-directory venv-location :parents))
+
+;;
+;; Emacs Prelude modules
+;;
+;; Prelude has its own way of configuring modules, badly enough
+;; it is not based on use-package, which seems to be the standard.
+;; Anyway, the following modules include lots of interesting
+;; configurations that are worth having; actually, most of them
+;; are 99% the Prelude experience.
+;;
+;; These are supposed to be in `.emacs.d/prelude-modules.el` but I
+;; like having a centralized configuration.
+;;
+;; Python module:
+;; Prelude ships with `anaconda-mode` which I don't like. That's why
+;; that module is disabled and `elpy` is enabled instead with
+;; `use-package`.
+;;
+(require 'prelude-ido)
+(require 'prelude-helm)
+(require 'prelude-helm-everywhere)
+(require 'prelude-company)
+(require 'prelude-programming)
+(require 'prelude-common-lisp)
+(require 'prelude-emacs-lisp)
